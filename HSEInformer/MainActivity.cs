f0ca0f7c14821544460227a9f64ccfe0be3b22e2
Сергей Бучко <sabuchko@edu.hse.ru>
2017-06-10 16:27:01 +0300

    Drawer

--- HSEInformer/MainActivity.cs
+++ HSEInformer/MainActivity.cs
@@ -4,10 +4,12 @@ using Android.OS;
 using Android.Support.V7.Widget;
 using SupportToolbar = Android.Support.V7.Widget.Toolbar;
 using Android.Support.V7.App;
+using Android.Support.Design.Widget;
 using Android.Content;
 using Android.Preferences;
 using Android.Net;
 using Android.Support.V4.Widget;
+using Android.Views;
 
 namespace HSEInformer
 {
@@ -15,20 +17,21 @@ namespace HSEInformer
     public class MainActivity : AppCompatActivity
     {
 
-        ISharedPreferences prefs;
-        ISharedPreferencesEditor editor;
-        private SupportToolbar toolbar;
-        private DrawerLayout drawerLayout;
-        //private NavigationView navigationView;
+        ISharedPreferences _prefs;
+        ISharedPreferencesEditor _editor;
+        SupportToolbar _toolbar;
+        DrawerLayout _drawerLayout;
+        NavigationView _navigationView;
+        ActionBarDrawerToggle _drawerToggle;
 
 
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
-            prefs = PreferenceManager.GetDefaultSharedPreferences(ApplicationContext);
-            editor = prefs.Edit();
-            editor.PutString("host", "http://hseinformerserver.azurewebsites.net");
-            editor.Apply();
+            _prefs = PreferenceManager.GetDefaultSharedPreferences(ApplicationContext);
+            _editor = _prefs.Edit();
+            _editor.PutString("host", "http://hseinformerserver.azurewebsites.net");
+            _editor.Apply();
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
@@ -50,21 +53,87 @@ namespace HSEInformer
         public void CustomizeToolbarAndNavView()
         {
             //Accepting toolbar and drawerlayout
-            toolbar = FindViewById<SupportToolbar>(Resource.Id.toolbar);
-            SetSupportActionBar(toolbar);
+            _toolbar = FindViewById<SupportToolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(_toolbar);
 
-            drawerLayout = FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
+            _drawerLayout = FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
+            _drawerToggle = new ActionBarDrawerToggle(this, _drawerLayout,
+                Resource.String.openDrawer, Resource.String.closeDrawer);
+            _drawerLayout.AddDrawerListener(_drawerToggle);
 
-            SupportActionBar.SetHomeButtonEnabled(true);
-            //SupportActionBar.SetDisplayHomeAsUpEnabled(true);
+            SupportActionBar.SetDisplayHomeAsUpEnabled(true);
             SupportActionBar.SetDisplayShowTitleEnabled(false);
+            _drawerToggle.SyncState();
+            _navigationView = FindViewById<NavigationView>(Resource.Id.nav_view);
+            _navigationView.NavigationItemSelected += _navigationView_NavigationItemSelected;
         }
 
 
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            _navigationView.InflateMenu(Resource.Menu.nav_menu);
+            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
+
+            return base.OnPrepareOptionsMenu(menu);
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            _drawerToggle.OnOptionsItemSelected(item);
+            switch (item.ItemId)
+            {
+                case (Resource.Id.menu_plus):
+                    {
+                        //TODO: Open add activity
+                        return true;
+                    }
+                default:
+                    return base.OnOptionsItemSelected(item);
+            }
+        }
+
+        private void _navigationView_NavigationItemSelected(object sender, NavigationView.NavigationItemSelectedEventArgs e)
+        {
+            switch (e.MenuItem.ItemId)
+            {
+                case Resource.Id.nav_exit:
+                    {
+                        //var dialog = new Android.App.AlertDialog.Builder(this);
+                        //dialog.SetMessage(GetString(Resource.String.logout_message));
+
+                        //dialog.SetPositiveButton("Yes", delegate { LogOut(); });
+                        //dialog.SetNegativeButton("Cancel", delegate { });
+                        //dialog.Show();
+                        //break;
+                        Toast.MakeText(this, "Выход", ToastLength.Long).Show();
+                        break;
+                    }
+                case Resource.Id.nav_profile:
+                    {
+                        Toast.MakeText(this, "Профиль", ToastLength.Long).Show();
+                        break;
+                    }
+                case Resource.Id.nav_content:
+                    {
+                        Toast.MakeText(this, "Новости", ToastLength.Long).Show();
+
+                        break;
+                    }
+                case Resource.Id.nav_deadlines:
+                    {
+                        Toast.MakeText(this, "Дедлайны", ToastLength.Long).Show();
+                        break;
+                    }
+            }
+            e.MenuItem.SetChecked(true);
+            _drawerLayout.CloseDrawers();
+        }
+
         public bool CheckAuthorization()
         {
-            var authorized = prefs.GetBoolean("authorized", false);
-            var token = prefs.GetString("token", null);
+            var authorized = _prefs.GetBoolean("authorized", false);
+            var token = _prefs.GetString("token", null);
 
             return true;
             CheckConnection();
