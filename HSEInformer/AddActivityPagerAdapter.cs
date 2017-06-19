63d708f7d4062139dccb8c5499f88fd62ab712da
Сергей Бучко <sabuchko@edu.hse.ru>
2017-06-19 09:04:25 +0300

    Plus

---
+++ HSEInformer/AddActivityPagerAdapter.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Support.V4.App;
+using Android.Preferences;
+using HSEInformer.Fragments.AddAndSearch;
+using Java.Lang;
+
+namespace HSEInformer
+{
+    class AddActivityPagerAdapter : FragmentStatePagerAdapter
+    {
+        List<Android.Support.V4.App.Fragment> fragments;
+        List<string> titles;
+        ISharedPreferences _prefs;
+        ApiManager _manager;
+        Context context;
+
+        public AddActivityPagerAdapter(Android.Support.V4.App.FragmentManager fm, Context context) : base(fm)
+        {
+            this.context = context;
+            _prefs = PreferenceManager.GetDefaultSharedPreferences(context);
+            FillTabs();
+        }
+
+        public void FillTabs()
+        {
+            var host = _prefs.GetString("host", null);
+            _manager = new ApiManager(host);
+            var token = _prefs.GetString("token", null);
+
+            fragments = new List<Android.Support.V4.App.Fragment>
+            {
+                InvitesFragment.newInstance(),
+                SearchGroupFragment.newInstance(),
+                CreateGroupFragment.newInstance()
+            };
+            titles = new List<string>
+            {
+               "Приглашения",
+               "Найти",
+               "Добавить"
+            };
+        }
+
+
+        public override Android.Support.V4.App.Fragment GetItem(int position)
+        {
+            return fragments[position];
+        }
+
+        public override ICharSequence GetPageTitleFormatted(int position)
+        {
+            return new Java.Lang.String(titles[position]);
+        }
+
+        public override int Count
+        {
+            get
+            {
+                return fragments != null ? fragments.Count : 0;
+            }
+        }
+    }
+
+}
